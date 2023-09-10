2914caf88a6c2dfd4dae02cf56134bc668a151b5
Nathan Quach <nquach0515@gmail.com>
2023-09-09 23:17:26 -0400

    JavaScript Intro Assessment

---
+++ script.js
@@ -0,0 +1,7 @@
+var firstName = "Nathan";
+var lastName = "Quach";
+var age = 19;
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("I'm having so much fun in this class.");
